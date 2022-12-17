ad8354a5fde9d17ebd9f32481a539721cc8e44e9
PPoznanski <marvin981x@gmail.com>
2022-12-17 21:00:41 +0100

    Add default role to registration

--- app/Http/Controllers/ViewGradeHistoryController.php
+++ app/Http/Controllers/ViewGradeHistoryController.php
@@ -16,8 +16,8 @@ class ViewGradeHistoryController extends Controller
         $data = Grade_change::join('grades','grades.id','=','grade_change.id_grade')
         ->join('users','users.id','=','grades.id_user')
         ->get(['grades.grade_value','grade_change.previous_grade', 'grades.comment', 'grade_change.history_created_at', 'grades.created_at'])
-        -where('users.id','=', $user = Auth::user()->id);
-        return view('grade_history', compact('previous_grade','data'));
+        ->where('users.id','=', $user = Auth::user()->id);
+        return view('grade_history', compact('data'));
 
 
         }
